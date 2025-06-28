ca55d05ad68b57f55928865cd42d0c1fee87ba4d
Chaitanya Anand <chaitanya.anand@gmail.com>
2025-06-28 23:05:04 +0530

    Add new participant images and update participant details in Participants component

--- src/components/Participants.tsx
+++ src/components/Participants.tsx
@@ -25,7 +25,7 @@ const Participants = () => {
     {
       name: "Eureka Araujo",
       img: "/judge2.png",
-      text: "Born in Mumbai &amp; grown up in Goa, Chef Eureka graduated in 2012 from the Institute of Hotel Management, Catering Technology &amp; Applied Nutrition, Goa. Later in 2014 she did her Taj Management Training Program, a Training program by the Taj group of hotels that is comparable to an MBA in Hospitality Food Production that moulds young chefs into future inspiring professionals.  Thereafter she worked at the Umaid Bhawan Palace as Senior Chef-De-Partie Bakery, Patisserie &amp; Confectionery, for 2 years during 2014-16. Chef Eureka joined the Academy of Pastry &amp; Culinary Arts (APCA) in 2016 as a Pastry Chef Instructor and went on to be promoted as the Executive Pastry Chef at the Mumbai Centre. <br/> She represented India at the ‘World Pastry Queen’ championship in Rimini Italy in 2018, through a direct entry by winning Gold at the ‘India Pastry Queen’ competition in 2017. Her candidature was sponsored by Academy of Pastry &amp; Culinary Arts (APCA). She also represented India in 2017 as the coach/judge of team India&#39;s Junior team championship in Rimini Italy.<br/> After leaving APCA in 2022 Chef Eureka started SIVAKO. As the Creative Director at Sivako, a french patisserie cloud kitchen in Mumbai, her mission was to develop people for the best in French pastry by creating  masterpieces through sight &amp; taste. <br/> Later she once again got the chance to represent India by becoming ‘India Pastry Queen’ at the ‘World Pastry Queen’ championship in 2023, where she won the Bronze Medal, among the best from around the world. During her tenure at APCA Chef Eureka got the opportunity to learn from multiple master chefs from around the world including Chef Johan Martin, Chef Jean Francois Arnald, Chef Amaury Guichon, Chef Wei Loon Tan, Chef Christophe Morel, and Chef Emmanuele Forcone to name few. She participated in various workshops on Sugar Display, Chocolate &amp; Pralines, Viennoiserie &amp; Tarts and modern French Pastries to further hone up her skills.",
+      text: "Born in Mumbai &amp; grown up in Goa, Chef Eureka graduated in 2012 from the Institute of Hotel Management, Catering Technology &amp; Applied Nutrition, Goa. Later in 2014 she did her Taj Management Training Program, a Training program by the Taj group of hotels that is comparable to an MBA in Hospitality Food Production that moulds young chefs into future inspiring professionals.  Thereafter she worked at the Umaid Bhawan Palace as Senior Chef-De-Partie Bakery, Patisserie &amp; Confectionery, for 2 years during 2014-16. Chef Eureka joined the Academy of Pastry &amp; Culinary Arts (APCA) in 2016 as a Pastry Chef Instructor and went on to be promoted as the Executive Pastry Chef at the Mumbai Centre. <br/> She represented India at the 'World Pastry Queen' championship in Rimini Italy in 2018, through a direct entry by winning Gold at the 'India Pastry Queen' competition in 2017. Her candidature was sponsored by Academy of Pastry &amp; Culinary Arts (APCA). She also represented India in 2017 as the coach/judge of team India's Junior team championship in Rimini Italy.<br/> After leaving APCA in 2022 Chef Eureka started SIVAKO. As the Creative Director at Sivako, a french patisserie cloud kitchen in Mumbai, her mission was to develop people for the best in French pastry by creating  masterpieces through sight &amp; taste. <br/> Later she once again got the chance to represent India by becoming 'India Pastry Queen' at the 'World Pastry Queen' championship in 2023, where she won the Bronze Medal, among the best from around the world. During her tenure at APCA Chef Eureka got the opportunity to learn from multiple master chefs from around the world including Chef Johan Martin, Chef Jean Francois Arnald, Chef Amaury Guichon, Chef Wei Loon Tan, Chef Christophe Morel, and Chef Emmanuele Forcone to name few. She participated in various workshops on Sugar Display, Chocolate &amp; Pralines, Viennoiserie &amp; Tarts and modern French Pastries to further hone up her skills.",
     },
     {
       name: "Parvinder Singh",
@@ -36,54 +36,53 @@ const Participants = () => {
 
   const pp = [
     {
-      img1: "/pp/8.png",
-      img2: "/pp/9.png",
-      name: "Sohan Kumar",
-      desc: "Sohan Kumar, a seasoned bread and pastry maker with over 14 years of experience, is currently a Demi Chef De Partie at Café Delhi Heights, New Delhi. Renowned for his expertise in French pastries, Western desserts, and a variety of European and Indian breads, Sohan consistently delivers high-quality creations. His innovative approach to dessert-making and dedication to excellence earned him the Employee of the Month award in 2019. With a strong focus on customer satisfaction and a proven track record in baking operations, Sohan is ready to showcase his skills in the Master Bakers challenge.",
+      img1: "/participants/Diksha Khandelwal.png",
+      img2: "/participants/ibca.png",
+      name: "Diksha Khandelwal",
+      desc: "A decorated pastry artist and educator, Diksha serves as Chef Instructor and Centre Head at the Institute of Bakery and Culinary Arts. With accolades including Gold at Aahar 2024 and the Nari Ratan Award, her journey reflects technical mastery and a passion for teaching. Internationally certified, she specializes in patisserie with deep roots in French culinary arts.",
     },
     {
-      img1: "/pp/14.png",
-      img2: "/pp/15.png",
-      name: "Bidur Kumar Mahapatra",
-      desc: "Bidur Kumar Mahapatra, a dedicated culinary enthusiast, currently works for Azzure Hospitality Pvt. Ltd. With a B.Sc. in Hospitality and Hotel Administration from Chitkara School of Hospitality, he has honed his skills through hands-on experience, including a 12-week industrial training at JW Marriott Chandigarh. An award-winning participant in Vancouver Island University's True Colors and Resolution for Hospitality, Bidur has also showcased his culinary talent in various competitions, including the Chef of the Year competition. Passionate about food production and service, Bidur is also a food blogger and content creator on Instagram, sharing his culinary journey through engaging food reels.",
+      img1: "/participants/Dinesh Kumar.png",
+      img2: "/participants/mrskaurs.png",
+      name: "Dinesh Kumar",
+      desc: "With a rich career spanning top bakeries in India and the Middle East, Dinesh Kumar brings mastery in artisanal baking and operational leadership. From innovating at Defense Bakery to scaling quality at Mrs Kaur's, he's a specialist in viennoiserie, sourdoughs, and team training—backed by international credentials and a passion for excellence.",
     },
     {
-      img1: "/pp/16.png",
-      img2: "/pp/17.png",
-      name: "Ajendra Singh",
-      desc: "Ajendra Singh is a highly skilled Senior Sous Chef at Sandal Suites, Noida, with over 15 years of experience in culinary excellence. He has honed his craft at renowned establishments such as The Westin Gurgaon and Pullman Central Park, Gurgaon. Ajendra's expertise in bakery and pastry, along with his meticulous approach to kitchen operations and menu planning, sets him apart. Awarded a Silver Medal in Petit Fours or Pralines at AAHAR Culinary Art India 2023, Ajendra is a distinguished chef whose creativity and dedication make him a formidable contender in the Master Bakers Challenge.",
+      img1: "/participants/Kanhaiya Lal Rawat.png",
+      img2: "/participants/lopera.png",
+      name: "Kanhaiya Lal Rawat",
+      desc: "With over 13 years at L'Opéra, Kanhaiya has risen from Commis III to Senior Chef de Partie at one of India's top French bakeries. He specializes in artisanal breads, pastries, and team operations, having worked alongside global pastry masters. His precision, leadership, and deep technical skills make him a standout in modern bakery execution.",
     },
     {
-      img1: "/pp/4.png",
-      img2: "/pp/5.png",
-      name: "Chandra Pratap Singh",
-      desc: "Chandra Pratap Singh brings over a decade of experience to the Master Bakers Challenge, honed at prestigious establishments such as Sheraton Hyderabad, Courtyard by Marriott Gurgaon, and Radisson Blu. With a Diploma in Hotel Management from Amrapali Institute, Chandra has excelled in roles including Commi and Demi Chef De Partie. Currently serving as a Demi Chef De Partie at Radisson Blu MBD Noida, he is skilled in maintaining high standards of cleanliness, managing inventory, and crafting a wide range of desserts, including specialty and sugar-free options, all while adhering to USPH and HACCP standards. His commitment to excellence and innovative baking techniques make him a standout competitor in the challenge.",
+      img1: "/participants/Mohd Shahnawaz.png",
+      img2: "/participants/cafedelhiheights.png",
+      name: "Mohd. Shahnawaz",
+      desc: "A veteran pastry chef with 20+ years of culinary experience, Shahnawaz currently heads pastry operations at Café Delhi Heights. From luxury resorts to fine-dining kitchens, he's honed his skills in artisan baking, costing, and kitchen leadership. Known for precision and consistency, his legacy spans landmark names like Tivoli Gardens, The Samrat Hotel, and Country Inn.",
     },
     {
-      img1: "/pp/2.png",
-      img2: "/pp/3.png",
-      name: "Anushka Madaan",
-      desc: "Anushka Madaan is a passionate baker and hospitality professional with a Diploma in Bakery, Pastry, and Entrepreneurship from the School for European Pastry, scoring an impressive 89.3%. She holds a B.Sc. in Hospitality and Hotel Administration from AIHM Chandigarh, where she achieved 82%. Anushka has hands-on experience from her internship at TAJ, Chandigarh, where she refined her bakery and patisserie skills and gained practical knowledge in various departments. She has led numerous projects, including the successful Rose Fest Carnival and Busy Bee bakery outlet. As a former Bakery Coordinator and school prefect, Anushka has demonstrated leadership and organizational skills. Her accolades include first prize in an inter-college cookery competition, and she has actively contributed to community service through the National Service Scheme and Rotaract. Anushka's creativity and dedication make her a standout participant in the Master Bakers Challenge India 2024.",
-    },
-    {
-      img1: "/pp/12.png",
-      img2: "/pp/13.png",
+      img1: "/participants/Sandeep.png",
+      img2: "/participants/claridges.png",
       name: "Sandeep Kumar",
-      desc: "Sandeep Kumar, a seasoned baker with over 12 years of experience, is currently a Junior Sous in Bakery at The Claridges, New Delhi. Sandeep’s culinary journey includes roles at prestigious establishments such as Pullman and Novotel Airocity, The Taj Palace, and Shangri-La Eros Hotel. His professional qualifications include a two-year apprenticeship in Bakery and Confectionery from Centaur Hotel, New Delhi, and extensive training at Cafair Flight Catering and The Grand Hotel. Known for his exceptional knife skills, in-depth product knowledge, and adherence to food safety and hygiene standards, Sandeep is eager to showcase his baking prowess at the Master Bakers Challenge India 2024.",
+      desc: "Currently Jr. Sous Chef at Hotel Claridges, Sandeep brings over 12 years of expertise in bakery and confectionery across top properties like Pullman, Taj Palace, and Shangri-La. Trained at Centaur Hotel and Grand New Delhi, he blends technical precision with operational excellence. Honored with the Master Baker Award 2024, he exemplifies consistency and culinary craft.",
     },
     {
-      img1: "/pp/10.png",
-      img2: "/pp/11.png",
-      name: "Prerna Kothari",
-      desc: "Prerna, a Senior Learning Facilitator at Le Cordon Bleu India, embarked on her culinary journey with a Bachelor's degree in Culinary Arts. She refined her skills at renowned establishments like ITC Royal Bengal and JW Marriott Kolkata, contributing to their culinary excellence. Her remarkable achievements include winning the prestigious Junior Pastry India Cup and representing Team India at the Junior Pastry World Championship at SIGEP. Prerna's commitment to innovation and creativity shines through in her breathtaking edible creations. She delights in working with chocolate and sugar, pushing the boundaries of traditional pastry techniques to craft awe-inspiring delicacies.",
+      img1: "/participants/Saurav Singh.png",
+      img2: "/participants/jwmarriott.png",
+      name: "Saurav Singh",
+      desc: "A dedicated bakery professional, Saurav Singh has steadily risen through roles at Hyatt, Oberoi, and now JW Marriott Kolkata. Known for his mastery in sourdoughs, viennoiserie, and dessert presentation, he blends technical skill with operational focus. With formal training and award recognition, he represents the new wave of precision-driven talent in India's bakery kitchens.",
     },
     {
-      img1: "/pp/6.png",
-      img2: "/pp/7.png",
-      name: "Gul Mohammad",
-      desc: "Gul Mohammad, an accomplished bakery chef with over 15 years of experience, currently leads the bakery section at Café Delhi Height. His journey from a 3rd Commis to Chef de Partie showcases his dedication and growth in the field. Known for his innovative baking techniques and creative recipes, Gul has consistently produced high-quality pastries and baked goods. His tenure at Café Delhi Heights and Ciclo Café has refined his skills, ensuring maximum customer satisfaction. With a passion for excellence, Gul is set to impress at the Master Bakers challenge, bringing his rich experience and culinary artistry to the forefront.",
+      img1: "/participants/Siddhesh Undire.png",
+      img2: "/participants/itcmaratha.png",
+      name: "Siddhesh Undire",
+      desc: "A bakery specialist with experience at India's finest luxury hotels, Siddhesh has mastered buffet layouts, viennoiserie, and artisan presentations. Now at ITC Maratha, he's known for operational discipline and creativity—from brunch setups to chocolate sculptures. With prior roles at St. Regis and Grand Hyatt, he brings a refined, guest-centric approach to modern bakery execution.",
     },
-
+    {
+      img1: "/participants/Pradum.png",
+      img2: "/participants/bakehousecomfort.png",
+      name: "Pradum Yadav",
+      desc: "Focused and fast-rising, Pradum Yadav brings 5+ years of core bakery experience across brands like Café Delhi Heights and Oasis Baklawa. With expertise in bread craftsmanship and a reputation for reliability, he's known for precision and consistency on the line. Energetic and eager to grow, he represents the next wave of committed talent in commercial baking.",
+    },
   ];
 
   return (
