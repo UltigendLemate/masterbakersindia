1969c50419f7e8b4ec688fb2e854339b1ac3c0e8
Chaitanya Anand <chaitanya.anand@gmail.com>
2025-06-29 11:33:04 +0530

    past remove

--- src/components/Participants.tsx
+++ src/components/Participants.tsx
@@ -88,7 +88,7 @@ const Participants = () => {
   return (
     <div className="w-full" id="Jury">
       <h2 className="z-20 mx-auto text-5xl md:text-7xl text-primary mb-4  mt-10 text-center heading ">
-        Past Participants
+        Participants
       </h2>
 
       <div className="md:mx-24 mx-10 mt-10 grid grid-cols-1 md:grid-cols-2 gap-10">
